15d5cd9e2c55c21cb48a28584bfdf6432b394d0d
Amanda Baker <mandykatebaker@Amandas-MacBook-Pro.local>
2016-06-20 12:51:45 +1200

    added event listener to board elements

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,23 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
-function startGame () {
+
+function startGame ()  {
+    //addListeners(document.getElementsByClassName('board')[0].children)
+    var children = document.getElementsByClassName('board')[0].children
+    console.log(children)
+    addListeners(children)
+    }
+
+function addListeners (element) {
+    for (var i = 0; i < element.length; i++) {
+        //console.log(elements[i])
+    element[i].addEventListener('click', showCell);
+    }
 }
+
+function showCell (evt) {
+    //console.log(evt.target)
+    evt.target.classList.remove('hidden')
+}
+
+startGame()
