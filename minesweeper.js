0d43e7b40a8c7b88d033470b77da3e2b851b9c59
Amanda Baker <mandykatebaker@Amandas-MacBook-Pro.local>
2016-06-26 10:14:39 +1200

    added get col and get row function

--- minesweeper.js
+++ minesweeper.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
+var board = {
+  cells : [
+
+  ]
+};
+
 function startGame () {
   var boardChildren = document.getElementsByClassName('board')[0].children;
 
@@ -10,6 +16,24 @@ function startGame () {
   function addListeners(elem) {
   elem.addEventListener('click', showCell);
   elem.addEventListener('contextmenu', markCell);
+  }
+
+  function getRow(ele) {
+  var classNames = ele.classList;
+  for (var i = 0; i < classNames.length; i++) {
+    if (classNames[i].indexOf("row-") > -1) {
+      return (parseInt(classNames[i].split("row-").join("")));
+      }
+    }
+  }
+
+function getCol(ele) {
+  var classNames = ele.classList;
+  for (var i = 0; i < classNames.length; i++) {
+    if (classNames[i].indexOf("col-") > -1) {
+      return (parseInt(classNames[i].split("col-").join("")));
+      }
+    }
   }
 
   function showCell (evt) {
